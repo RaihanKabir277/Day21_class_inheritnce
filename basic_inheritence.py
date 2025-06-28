5158fa09ea467325d024a92a5b692494ab5a41b4
RaihanKabir277 <raihank0198@gmail.com>
2025-06-28 06:11:14 +0600

    inheritence added here , this is the basic code

--- basic_inheritence.py
+++ basic_inheritence.py
@@ -6,9 +6,20 @@ class Animal:
     def breathe(self):
         print("Inhale, Exhale")
 
-class Fish:
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    # ---------- if we want to inherit a method and want to modify oursleves then the code is given below------
+
+    def breathe(self):
+        super().breathe()
+        print("Doing under water ")
+
     def swim(self):
         print("Moving in Water.")
 
 nemo = Fish()
 nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
